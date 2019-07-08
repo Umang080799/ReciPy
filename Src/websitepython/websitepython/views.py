7c07f32a32d8c78305bb154dd3febecb5f616928
Umang Gupta <35209670+Umang080799@users.noreply.github.com>
2019-07-07 23:53:08 -0400

    Working Windows version

--- Src/websitepython/websitepython/views.py
+++ Src/websitepython/websitepython/views.py
@@ -2,13 +2,7 @@ from django.shortcuts import render
 import sys
 from pathlib import Path
 
-#It's the current file location
-HERE = Path(__file__).parent
 
-sys.path.append(str(HERE / '../../'))
-
-#Importing the main() function
-from main import main
 
 #For renderting the HTML page
 def button(request):
@@ -16,5 +10,10 @@ def button(request):
 
 #For displaying the output once the button is clicked
 def output(request):
+    #It's the current file location
+    HERE = Path(__file__).parent
+    sys.path.append(str(HERE / '../../'))
+    #Importing the main() function
+    from main import main
     search = main()
     return render(request,'home.html',{'search': search})
